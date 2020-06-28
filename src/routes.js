8bb8a775e6657b3cbffeeb4507118aa0a369047d
Hugo Franco Alves Gil <hugofranco_12@hotmail.com>
2020-06-27 21:45:39 -0300

    database routes added :ok_hand:

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
-
 const route = express.Router();
+const knex = require("./database/database");
 
 route.get("/", (req, res) => {
   res.send("hello World!");
@@ -12,7 +12,24 @@ route.post("/signup", (request, response) => {
 
   request.header("application/json");
   const form = request.body;
+
   response.json(form);
+});
+
+route.post("/database", async (req, res) => {
+  try {
+    const account = await knex("ACCOUNTS").insert(req.body, "*");
+    res.json(account);
+  } catch (err) {
+    res.status(400).json({
+      error: "Deu bosta jão",
+    });
+  }
+});
+
+route.get("/database", async (req, res) => {
+  const account = await knex("ACCOUNTS").select("*");
+  res.json(account);
 });
 
 module.exports = route;
