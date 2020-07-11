c7e51589770284d0a5c534d40efe560f5da9beee
Hugo Franco Alves Gil <hugofranco_12@hotmail.com>
2020-07-11 18:26:18 -0300

    esse controlador agora age como um controlador

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -1,39 +1,15 @@
-const express = require("express");
-const route = express.Router();
-const ValidationError = require("../errors/ValidationError");
-const validator = require("validator");
-const knex = require('../database/database');
+const Account = require("../model/userModel");
 
-class Account {
-  constructor(body) {
-    this.body = body;
-    this.user = null;
+exports.store = async (req, res, next) => {
+  const user = new Account(req.body);
+  try {
+    const accountCreated = await user.registrar();
+
+    return res
+      .status(201)
+      .json({ msg: "O Usuário foi criado com sucesso", accountCreated });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json("SQL Maping é feio em!");
   }
-
-  registrar() {
-    this.checkBodyKeys();
-    this.validate();
-
-    this.body = {
-      name: this.body.name,
-      email: this.body.email,
-      password: this.body.password,
-    };
-
-    this.user = // Função para criar usuário
-
-  }
-
-  checkBodyKeys() {
-    if (!this.body.nome) throw new Error("Nome é um dado necessário");
-    if (!this.body.email) throw new Error("Email é um dado necessário");
-    if (!this.body.password) throw new Error("Senha é um dado necessário");
-  }
-
-  validate() {
-    if (!this.validator.isEmail(email))
-      throw new ValidationError("O Email inserido é inválido.");
-    if (this.body.password.length < 6 || this.body.password.length > 16)
-      throw new ValidationError("Senha inválida");
-  }
-}
+};
