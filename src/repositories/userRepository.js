4a0b52d6a18e20077cfd5fc319c8e8d24c380f9c
Hugo Franco <hugofranco_12@hotmail.com>
2020-11-18 22:01:59 -0300

    repositorio adicionado para lidar com o DB

---
+++ src/repositories/userRepository.js
@@ -0,0 +1,17 @@
+const { __esModule } = require("validator/lib/isAlpha");
+const database = require("../database/database");
+
+
+// Função para criar usuário
+
+module.exports = function create (validatedUser) {
+     database("ACCOUNTS").insert(
+        {
+          NOME: this.validatedUser.name,
+          EMAIL: this.validatedUseremail,
+          PASSWORD: this.validatedUser.password,
+        },
+        ["*"]
+      )
+      return validatedUser;
+}
