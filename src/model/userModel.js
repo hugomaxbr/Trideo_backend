216a86a7cacf5db2d87caf5bd231bdadf29ccc39
Hugo Franco <hugofranco_12@hotmail.com>
2020-11-18 21:44:56 -0300

    Model sendo refeito para melhor arquitetura do projeto

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const route = express.Router();
 const ValidationError = require("../errors/ValidationError");
-const validator = require("validator");
+
 const knexfile = require("../../knexfile");
-const database = require("../database/database");
+
 
 class Account {
   constructor(body) {
@@ -11,40 +11,19 @@ class Account {
   }
 
   async registrar() {
-    this.checkBodyKeys();
-    this.validate();
-
+    
     this.body = {
       name: this.body.name,
       email: this.body.email,
       password: this.body.password,
     };
-    console.log(this.body);
-
-    // Função para criar usuário
-
-    return database("ACCOUNTS").insert(
-      {
-        NOME: this.body.name,
-        EMAIL: this.body.email,
-        PASSWORD: this.body.password,
-      },
-      ["*"]
-    );
+    
+    
   }
 
-  checkBodyKeys() {
-    if (!this.body.name) throw new Error("Nome é um dado necessário");
-    if (!this.body.email) throw new Error("Email é um dado necessário");
-    if (!this.body.password) throw new Error("Senha é um dado necessário");
-  }
+  
 
-  validate() {
-    if (!validator.isEmail(this.body.email))
-      throw new ValidationError("O Email inserido é inválido.");
-    if (this.body.password.length < 6 || this.body.password.length > 16)
-      throw new ValidationError("Senha inválida");
-  }
+  
 
   async login() {
     this.body = {
