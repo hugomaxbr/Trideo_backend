f418ce54988ad62924018faf8423a0c6ea0fbd66
Hugo Franco <hugofranco_12@hotmail.com>
2020-11-20 22:54:17 -0300

    função de validação movida para o controller, agora o create é executado só se o controller validar o formato dos dados

--- src/services/createUser.js
+++ src/services/createUser.js
@@ -1,12 +1,8 @@
-const Account = require("../model/userModel");
-const validateUser = require("../validators/validateUser");
 const userRepository = require("../repositories/userRepository");
 
 
 
 
-const execute = (vUser) => {
-    validation(vUser);
-    return create(vUser);
-    
+const execute = (user) => {
+    return create(user);
 }
