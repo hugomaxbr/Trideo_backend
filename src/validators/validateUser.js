e895a0312a8d2ced2b921b6aa77ee3d12a8f14ad
Hugo Franco <hugofranco_12@hotmail.com>
2020-11-20 22:55:31 -0300

    validate user agora valida sem precisar do this

--- src/validators/validateUser.js
+++ src/validators/validateUser.js
@@ -1,20 +1,16 @@
 const validator = require("validator");
-const Account = require("../model/userModel");
-const user = new Account(req.body);
 
-module.exports = function validation() {
-    if (!this.user.name) throw new ValidationErrorError("Nome é um dado necessário");
-    if (!this.user.email) throw new ValidationErrorError("Email é um dado necessário");
-    if (!this.user.password) throw new ValidationErrorError("Senha é um dado necessário");
-    if (!validator.isEmail(this.user.email))
-        throw new ValidationError("O Email inserido é inválido.");
-    if (this.user.password.length < 6 || this.user.password.length > 16)
+
+
+module.exports = function (user) {
+    if (user.name) throw new ValidationErrorError("Nome é um dado necessário");
+    if (user.email) throw new ValidationErrorError("Email é um dado necessário");
+    if (user.password) throw new ValidationErrorError("Senha é um dado necessário");
+    if (!validator.isEmail(user.email))
+        throw new ValidationError("Email inválido");
+    if (user.password.length < 6 || user.password.length > 16)
         throw new ValidationError("Senha inválida");
     else{
-        validatedUser = this.user;
-        return validatedUser;
+        return user;
     }
-     
-
-    
   };
